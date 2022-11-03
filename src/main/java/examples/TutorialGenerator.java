d3650fb6d262540025dfa5d7f5fd4c0e5624b154
Krab <jakubpodtrzitkorak@gmail.com>
2022-11-03 19:08:45 +0100

    transparent image test

--- src/main/java/examples/TutorialGenerator.java
+++ src/main/java/examples/TutorialGenerator.java
@@ -2,10 +2,12 @@ package examples;
 
 import lazy.LazyGui;
 import processing.core.PApplet;
+import processing.core.PGraphics;
 
 @SuppressWarnings("IntegerDivisionInFloatingPointContext")
 public class TutorialGenerator extends PApplet {
     LazyGui gui;
+    PGraphics pg;
 
     public static void main(String[] args) {
         PApplet.main(java.lang.invoke.MethodHandles.lookup().lookupClass());
@@ -18,22 +20,33 @@ public class TutorialGenerator extends PApplet {
 
     public void setup() {
         gui = new LazyGui(this);
+        pg = createGraphics(width, height, P2D);
     }
 
     public void draw() {
         background(gui.colorPicker("background").hex);
-        float x = gui.slider("my circle/x");
-        fill(gui.colorPicker("circle/fill").hex);
-        stroke(gui.colorPicker("circle/stroke").hex);
-        strokeWeight(gui.slider("circle/weight", 3));
-        ellipse(x, height/2, 50, 50);
+        pg.beginDraw();
+        drawScene();
+        pg.endDraw();
+        image(pg, 0, 0, width, height);
+    }
 
+    private void drawScene() {
+        float x = gui.slider("my circle/x");
+        pg.clear();
+        pg.fill(gui.colorPicker("circle/fill").hex);
+        pg.stroke(gui.colorPicker("circle/stroke").hex);
+        pg.strokeWeight(gui.slider("circle/weight", 3));
+        pg.ellipse(x, height/2, 50, 50);
     }
 
     public void keyPressed() {
         if(key == 's'){
-            image(gui.getGuiCanvas(), 0, 0);
-            save("C:\\Projects\\LazyGui\\readme_assets\\slider.png");
+            pg.beginDraw();
+            drawScene();
+            pg.image(gui.getGuiCanvas(), 0, 0);
+            pg.endDraw();
+            pg.save("C:\\Projects\\LazyGui\\readme_assets\\slider.png");
         }
     }
 }
