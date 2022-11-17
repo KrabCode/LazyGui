a97bec0a69155c53802a94389c5a47f3d44d7829
Krab <jakubpodtrzitkorak@gmail.com>
2022-11-17 20:51:56 +0100

    prettier string pickers are now toggles

--- src/main/java/lazy/StringPickerItem.java
+++ src/main/java/lazy/StringPickerItem.java
@@ -1,10 +1,5 @@
 package lazy;
 
-import processing.core.PGraphics;
-
-import static lazy.State.cell;
-import static processing.core.PConstants.*;
-
 class StringPickerItem extends ToggleNode {
 
     String valueString;
@@ -13,21 +8,6 @@ class StringPickerItem extends ToggleNode {
         super(path, folder, valueBoolean);
         this.type = NodeType.TRANSIENT;
         this.valueString = valueString;
-    }
-
-    @Override
-    protected void updateDrawInlineNodeAbstract(PGraphics pg){
-        // do not call super() to not draw the right toggle handle
-        if(valueBoolean){
-            pg.noStroke();
-            pg.fill(ThemeStore.getColor(ThemeColorType.FOCUS_BACKGROUND));
-            pg.rectMode(CORNER);
-            pg.rect(1,0,size.x-1,size.y);
-            pg.fill(ThemeStore.getColor(ThemeColorType.FOCUS_FOREGROUND));
-            float rectSize = cell * 0.25f;
-            pg.rectMode(CENTER);
-            pg.rect(size.x - cell*0.5f, size.y - cell*0.5f, rectSize, rectSize);
-        }
     }
 
     @Override
