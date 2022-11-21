5fa0860d9cb95287922a89fe8ceefa757abce597
Jakub Rak <rak@fss.eu>
2022-11-21 11:05:52 +0100

    cleaned up color picker example

--- src/main/java/examples/ColorPickerExample.java
+++ src/main/java/examples/ColorPickerExample.java
@@ -20,13 +20,12 @@ public class ColorPickerExample extends PApplet {
 
     @SuppressWarnings("DuplicatedCode")
     public void draw() {
-        background(gui.colorPicker("background", color(20)).hex);
-        fill(gui.colorPicker("foreground", color(200)).hex);
-        gui.colorPickerHueAdd("foreground", gui.slider("foreground hue +"));
-        gui.slider("test/input 1");
-        gui.sliderInt("test/input 2");
-        gui.stringInput("test/input text", "hello world");
-        noStroke();
+        String path = "scene/";
+        background(gui.colorPicker(path + "background", color(20)).hex);
+        fill(gui.colorPicker(path + "rect/fill", color(200)).hex);
+        gui.colorPickerHueAdd(path + "rect/fill", gui.slider(path + "rect/fill hue +"));
+        stroke(gui.colorPicker(path + "rect/stroke").hex);
+        strokeWeight(gui.slider(path + "rect/weight", 10));
         rectMode(CENTER);
         rect(width/2f, height/2f, 150, 150);
     }
