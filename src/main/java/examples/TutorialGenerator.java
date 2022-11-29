471157787cb43fa62b655f76cf1603706e068787
Krab <jakubpodtrzitkorak@gmail.com>
2022-11-29 21:38:04 +0100

    made the tutorial generator make sense with the new manual gui.draw() improvements

--- src/main/java/examples/TutorialGenerator.java
+++ src/main/java/examples/TutorialGenerator.java
@@ -4,6 +4,7 @@ import lazy.LazyGui;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PImage;
+import processing.core.PVector;
 
 import java.awt.*;
 import java.io.File;
@@ -27,37 +28,38 @@ public class TutorialGenerator extends PApplet {
     }
 
     public void settings() {
-        size(1000,1000,P2D);
+        size(1000, 1000, P2D);
         noSmooth();
     }
 
     public void setup() {
         pointer = loadImage("C:\\Users\\Krab\\Documents\\GitHub\\LazyGui\\readme_assets\\cursor.png");
         gui = new LazyGui(this);
-        unregisterMethod("draw", gui);
         pg = createGraphics(width, height, P2D);
     }
 
     public void draw() {
-        noCursor();
-        gui.draw();
         clear();
+        noCursor();
         drawScene();
+        gui.draw();
+        image(pointer, mouseX + gui.slider("pointer/y"), mouseY + gui.slider("pointer/x"));
+        record();
     }
 
     private void drawScene() {
-        float x = gui.slider("folder/x");
         pg.beginDraw();
         pg.background(gui.colorPicker("background").hex);
+        PVector offset = gui.plotXY("circle/translate");
         pg.fill(gui.colorPicker("circle/fill").hex);
         pg.stroke(gui.colorPicker("circle/stroke").hex);
         pg.strokeWeight(gui.slider("circle/weight", 3));
-        pg.ellipse(x, height/2, 50, 50);
-        pg.image(gui.getGuiCanvas(), 0, 0);
-        pg.image(pointer, mouseX+gui.slider("pointer/y"), mouseY+gui.slider("pointer/x"));
+        pg.pushMatrix();
+        pg.translate(width / 2f + offset.x, width / 2f + offset.y);
+        pg.ellipse(0, 0, 50, 50);
+        pg.popMatrix();
         pg.endDraw();
         image(pg, 0, 0, width, height);
-        record();
     }
 
 
@@ -65,7 +67,7 @@ public class TutorialGenerator extends PApplet {
         return UUID.randomUUID().toString().replace("-", "").substring(0, 8);
     }
 
-    public void record(){
+    public void record() {
         gui.pushFolder("rec");
         int recLength = gui.sliderInt("frames", 600);
         if (gui.button("start")) {
@@ -79,12 +81,12 @@ public class TutorialGenerator extends PApplet {
         }
 
         String sketchMainClassName = getClass().getSimpleName();
-        String recDir = "out/rec/" + sketchMainClassName +"_" + recordingId;
+        String recDir = "out/rec/" + sketchMainClassName + "_" + recordingId;
         String recDirAbsolute = Paths.get(recDir).toAbsolutePath().toString();
-        if(gui.button("open folder")){
+        if (gui.button("open folder")) {
             Desktop desktop = Desktop.getDesktop();
             File dir = new File(recDirAbsolute + "\\");
-            if(!dir.exists()){
+            if (!dir.exists()) {
                 dir.mkdirs();
             }
             try {
@@ -97,10 +99,10 @@ public class TutorialGenerator extends PApplet {
         int recordRectPosY = height / 2 + gui.sliderInt("rect center y", 0);
         int recordRectSizeX = gui.sliderInt("rect size x", height / 2);
         int recordRectSizeY = gui.sliderInt("rect size y", height / 2);
-        if(recordRectSizeX % 2 != 0){
+        if (recordRectSizeX % 2 != 0) {
             recordRectSizeX += 1;
         }
-        if(recordRectSizeY % 2 != 0){
+        if (recordRectSizeY % 2 != 0) {
             recordRectSizeY += 1;
         }
         String recImageFormat = ".jpg";
@@ -112,9 +114,9 @@ public class TutorialGenerator extends PApplet {
                     recordRectSizeX,
                     recordRectSizeY
             );
-            cutout.save( recDir + "/" + saveIndex++ + recImageFormat);
+            cutout.save(recDir + "/" + saveIndex++ + recImageFormat);
         }
-        if(stopCommand || (recStarted != -1 && frameCount == recStarted + recLength)){
+        if (stopCommand || (recStarted != -1 && frameCount == recStarted + recLength)) {
             println("Recorded image series folder: " + recDirAbsolute);
         }
         if (gui.toggle("show rect")) {
@@ -127,10 +129,10 @@ public class TutorialGenerator extends PApplet {
         }
 
         int ffmpegFramerate = gui.sliderInt("ffmpeg fps", 60, 1, Integer.MAX_VALUE);
-        if(gui.button("ffmpeg make mp4")){
+        if (gui.button("ffmpeg make mp4")) {
             String outMovieFilename = recDirAbsolute + "/_" + generateRandomShortId();
             String inputFormat = recDirAbsolute + "/%01d" + recImageFormat;
-            String command = String.format("ffmpeg  -r " + ffmpegFramerate +" -i %s -start_number_range 100000 -an %s.mp4",
+            String command = String.format("ffmpeg  -r " + ffmpegFramerate + " -i %s -start_number_range 100000 -an %s.mp4",
                     inputFormat, outMovieFilename);
             println("running ffmpeg: " + command);
             try {
