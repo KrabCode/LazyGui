46013d8d2cf0023e5b2a2019b619058a5a44663d
Krab <jakubpodtrzitkorak@gmail.com>
2023-01-30 01:07:49 +0100

    class renamed

--- src/main/java/examples/TypeConflictTest.java
+++ src/main/java/examples/TypeConflictTest.java
@@ -4,7 +4,7 @@ import processing.core.PApplet;
 import processing.core.PGraphics;
 import lazy.LazyGui;
 
-public class CrowdedPathTest extends PApplet {
+public class TypeConflictTest extends PApplet {
     LazyGui gui;
     PGraphics pg;
 
