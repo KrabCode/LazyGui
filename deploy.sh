90f010ba92b298a2e877f70daeea41ec09e597f2
Krab <jakubpodtrzitkorak@gmail.com>
2023-04-02 12:20:47 +0200

    small adjustments to wording in properties

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,7 @@ rm -rf $t/
 echo "Deploying LazyGui to $t..."
 mkdir -p $t/data/ && cp -r data/ $t/
 mkdir -p $t/src/ && cp -r src $t/
-
-mkdir -p $t/reference/ && cp -r docs/ $t/reference
+mkdir -p $t/reference/ && cp -r docs/* $t/reference
 mkdir -p $t/examples/ && cp -r src/main/java/com/krab/lazy/examples $t/
 mkdir -p $t/library/ && cp out/artifacts/LazyGui.jar $t/library/LazyGui.jar
 cp library.properties $t/library.properties
