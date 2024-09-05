1f47bbf9acd320caa581d1164befcb8fe4b6e865
KrabCode <krabcode@gmail.com>
2024-09-06 00:23:57 +0200

    more javadocs yay

--- src/main/java/com/krab/lazy/ColorPoint.java
+++ src/main/java/com/krab/lazy/ColorPoint.java
@@ -1,13 +1,19 @@
 package com.krab.lazy;
 
 /**
- * Used for initializing a gradient with a set of default colors at default positions.
- * See LazyGui.gradient(String, ColorPoint...) for more information.
+ * Used as a parameter for initializing a gradient with a set of default colors at default positions.
+ * @see LazyGui#gradient(String, ColorPoint...) gradient function where it is used
  */
 public class ColorPoint{
-    int color; // processing hex color, e.g. 0xFF00FF00
-    float position; // expected to be normalized to [0, 1]
+    int color; //
+    float position; //
 
+    /**
+     * Constructor for ColorPoint
+     * @param color processing hex color like 0xFF00FF00 or color(255, 0, 0)
+     * @param position expected to be in the range [0, 1]
+     * @see LazyGui#colorPoint(int, float) more convenient way to create a ColorPoint.
+     */
     public ColorPoint(int color, float position){
         this.color = color;
         this.position = position;
